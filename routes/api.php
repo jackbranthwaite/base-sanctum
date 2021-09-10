385797816ba3a7e08215951870ee6d538b030db7
Jack Branthwaite <jack@formfunfunction.com>
2021-09-10 14:04:30 +1200

    Get and post request functioning

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::post('/products', [ProductController::class, 'store']);
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
