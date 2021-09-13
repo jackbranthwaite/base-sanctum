a2aa065de03a7268b6ef4afeb336e0b3cfb217c8
Jack Branthwaite <jack@formfunfunction.com>
2021-09-13 16:51:19 +1200

    Some progress made on combining sanctum and fortify

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,11 +21,16 @@ class RedirectIfAuthenticated
     {
         $guards = empty($guards) ? [null] : $guards;
 
+
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
+                if ($request->expectsJson()) {
+                    return response()->json(['error' => 'Already authenticated.'], 200);
+                }
                 return redirect(RouteServiceProvider::HOME);
             }
         }
+
 
         return $next($request);
     }
