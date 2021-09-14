0fb7e293ac49b9e254d5a294bf5c0760db1579ef
Jack Branthwaite <jack@formfunfunction.com>
2021-09-14 17:44:33 +1200

    Attempt to fix return of 422 when registering

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,13 +12,17 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $fields = $request->validate([
-            'name' => 'required|string',
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'role' => 'required|string',
             'email' => 'required|string|unique:users,email',
             'password' => 'required|string|confirmed'
         ]);
 
         $user = User::create([
-            'name' => $fields['name'],
+            'first_name' => $fields['first_name'],
+            'last_name' => $fields['last_name'],
+            'role' => $fields['role'],
             'email' => $fields['email'],
             'password' => bcrypt($fields['password'])
         ]);
